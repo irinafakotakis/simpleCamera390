1fa39f5abec6c7d4e5b54b98e16e1c2b183ca19b
MBenchimol <32246260+MBenchimol@users.noreply.github.com>
2019-03-28 14:28:36 -0400

    #98 test if script runs with purposeful error in app.kt

--- app/src/main/kotlin/com/simplemobiletools/camera/App.kt
+++ app/src/main/kotlin/com/simplemobiletools/camera/App.kt
@@ -6,6 +6,6 @@ import com.simplemobiletools.commons.extensions.checkUseEnglish
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
-        checkUseEnglish()
+        checkUseEnglish(
     }
 }
