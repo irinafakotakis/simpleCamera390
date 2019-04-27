da3734ddbbe14dda6873d08388b0f4685b6d0a71
Keeran10 <keeran.r10@gmail.com>
2019-04-27 15:01:58 -0400

    [#98] attempt to generate surefire report from intentional travis failure

---
+++ app/src/test/java/com/simplemobiletools/camera/activities/TravisCITests.java
@@ -0,0 +1,12 @@
+package com.simplemobiletools.camera.activities;
+
+import org.junit.Test;
+
+public class TravisCITests {
+
+    // needed for travis error log
+    @Test
+    public void intentionalFail(){
+        assert(false);
+    }
+}
